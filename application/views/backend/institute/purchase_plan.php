4612d36a2b0cb97072787e35b0a49e2c31d83322
Ajmal-hassan <ajmalhassan651@gmail.com>
2020-04-25 22:59:58 +0500

    plan card design set

--- application/views/backend/institute/purchase_plan.php
+++ application/views/backend/institute/purchase_plan.php
@@ -1,3 +1,11 @@
+<style>
+.box-shadow1{
+  box-shadow: 0px 4px 20px -2px rgba(0,0,0,0.75)!important;
+}
+.card-header {
+    border-bottom: 1px solid #4e5bf2;
+}
+</style>
 <div class="row ">
   <div class="col-xl-12">
     <div class="card">
@@ -11,16 +19,16 @@
   <div class="col-xl-12">
     <div class="card">
       <div class="card-body">
-        <h4 class="header-title mb-3"><?php echo get_phrase('class_add_form'); ?></h4>
+        <h4 class="header-title mb-3"><?php echo get_phrase('choose_a_plan'); ?></h4>
         <div class="container">
               <div class="card-deck mb-3 text-center">
                 <?php foreach ($plans as $key => $plan) { ?>
-                  <div class="card mb-4 box-shadow">
+                  <div class="card mb-4 box-shadow box-shadow1">
                     <div class="card-header">
-                      <h4 class="my-0 font-weight-normal"><?php echo $plan['name']?></h4>
+                      <h4 class="my-0 font-weight-normal"><?php echo strtoupper($plan['name'])?></h4>
                     </div>
                     <div class="card-body">
-                      <h1 class="card-title pricing-card-title"><?php echo $plan['price']?> <small class="text-muted">/ mo</small></h1>
+                      <h1 class="card-title pricing-card-title"><?php echo $plan['price']?> <small class="text-muted">/ reg</small></h1>
                       <ul class="list-unstyled mt-3 mb-4">
                         <li><strong><?php echo $plan['courses']?></strong> No of Courses</li>
                         <li><strong><?php echo $plan['classes']?></strong> No of Classes</li>
@@ -30,7 +38,7 @@
                       </ul>
                       <form action="<?php echo site_url('institute/plan_price'); ?>" method="post">
                         <input type="hidden" name="plan_id" value="<?php echo $plan['id']; ?>" />
-                        <button class = "btn btn-success" type="submit" name="button"><?php echo get_phrase('buy_now'); ?></button>
+                        <button class = "btn btn-primary" type="submit" name="button"><?php echo get_phrase('buy_now'); ?></button>
                       </form>
                       <!-- <a href = "javascript::" class="btn btn-lg btn-block btn-primary" id = "plan_<?php echo $plan['id']; ?>" onclick="handlePlanNow(this)"><?php echo get_phrase('buy_now'); ?></a> -->
                     </div>
