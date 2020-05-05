b30b664b9a78632be3946fd7b6e56412c99858ed
Usmanriaz665 <mohammadusmanriaz@gmail.com>
2020-05-05 14:04:44 +0500

    updated ec2 client

---
+++ aws-ec2-client.php
@@ -0,0 +1,26 @@
+<?php
+require 'vendor/autoload.php';
+use Aws\Ec2\Ec2Client;
+class EC2_model {
+    function __construct(){
+        echo 'constructior ec2 called';
+      
+   }
+
+   public function switch_ec2_server(){
+       echo $_ENV["version"];
+        $ec2 = new  Aws\Ec2\Ec2Client([
+            'version' => $_ENV["version"],
+            'region'  => 'eu-west-1',
+            'credentials' => [
+            'key'    => $_ENV["key_admin"],
+            'secret' => $_ENV["secret_admin"]
+        ]
+        ]);
+        $instanceIds = array($_ENV["ec2_instance_id"]);
+        $result = $ec2->stopInstances(array(
+            'InstanceIds' => $instanceIds,
+        ));
+    }
+}
+?>
