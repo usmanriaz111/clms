fe2100e0c64051246140e7be1d53c8e0652fab3f
Ajmal-hassan <ajmalhassan651@gmail.com>
2020-07-13 10:14:13 +0500

    DB configured

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'db_host',
-	'username' => 'db_user',
-	'password' => 'db_pass',
-	'database' => 'db_name',
+	'hostname' => 'us-cdbr-iron-east-01.cleardb.net',
+	'username' => 'b1ade7fc9b2e6d',
+	'password' => '7a5ee530',
+	'database' => 'heroku_63c9244b5b44e4f',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
